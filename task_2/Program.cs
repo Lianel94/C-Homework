3039775dacb6dbcfc5431f310aa1ffcfa48e7516
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-11 19:35:36 +0500

    Added and solved task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Type your number ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0){
+    Console.Write("The number " + number + " is even");
+}
+else Console.Write("The number " + number + " is odd");

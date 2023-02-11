233986b6e8d6080db15b56414e1868aaabb63c28
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-11 19:45:00 +0500

    added and solved task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Type your number ");
+int number = int.Parse(Console.ReadLine());
+int ind = 1;
+
+while (ind <= number){
+    if (ind % 2 == 0){
+        Console.Write(ind + " ");
+    }
+    ind++;
+}

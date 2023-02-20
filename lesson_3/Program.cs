203057ed65b8a528fc6395f7aea9a6ba26234e11
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-20 13:34:38 +0500

    Lesson_3 task 2 solved

--- lesson_3/Program.cs
+++ lesson_3/Program.cs
@@ -3,6 +3,8 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.Clear();
+
 Console.Write("Type a 5-digit number: ");
 string userNumber = Console.ReadLine();
 
@@ -15,4 +17,22 @@ if(userNumber[0] == userNumber[4] && userNumber[1] == userNumber[3])
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.Write("Type X1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Type Y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Type Z1: ");
+double z1 = double.Parse(Console.ReadLine());
+
+Console.Write("Type X2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Type Y2: ");
+double y2 = double.Parse(Console.ReadLine());
+Console.Write("Type Z2: ");
+double z2 = double.Parse(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+Console.WriteLine($"The distance between two points is {distance:f2}");
 

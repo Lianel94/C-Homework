6feceac5e2e075c40528e56ac838abff37492630
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-20 13:18:50 +0500

    Lesson_3 1 task solved

--- lesson_3/Program.cs
+++ lesson_3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Type a 5-digit number: ");
+string userNumber = Console.ReadLine();
+
+if(userNumber[0] == userNumber[4] && userNumber[1] == userNumber[3])
+{
+	Console.WriteLine($"The number {userNumber} is a palindrome");
+} else Console.WriteLine($"The number {userNumber} is NOT a palindrome");
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+

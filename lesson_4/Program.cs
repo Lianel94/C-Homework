d78a08aa426fc0b5dd6832aa8d6e681d8ab4d531
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-23 17:51:42 +0500

    Lesson_4 task 3 solved

--- lesson_4/Program.cs
+++ lesson_4/Program.cs
@@ -59,3 +59,15 @@ Console.WriteLine($"The sum of the digits of {num} is {GetSum(num)}");
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+int[] GetArray(int size)
+{
+	int[] result = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		result[i] = new Random().Next(10, 51);
+	}
+	return result;
+}
+
+int[] array = GetArray(8);
+Console.WriteLine($"[{String.Join(", ", array)}]");

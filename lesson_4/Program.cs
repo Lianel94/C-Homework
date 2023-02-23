d4d02d0a51cc18618849275f8eb3bb219b429f2e
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-23 17:43:47 +0500

    Lesson_4 task 2 solved

--- lesson_4/Program.cs
+++ lesson_4/Program.cs
@@ -38,3 +38,24 @@ if(ValidatePower(userPower))
 // 82 -> 10
 // 9012 -> 12
 
+Console.Write("Type your number: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetSum(int num)
+{
+	int res = 0;
+	while(num > 0)
+	{
+		res += num % 10;
+		num = num / 10;
+	}
+	return res;
+}
+
+Console.WriteLine($"The sum of the digits of {num} is {GetSum(num)}");
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
